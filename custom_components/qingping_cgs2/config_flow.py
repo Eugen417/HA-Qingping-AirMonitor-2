d2ede1f0b828f74d6063ab8c972534bcb2d2cc94
Eugen417 <eugen417@gmail.com>
2026-06-14 21:06:52 +0300

    Update config_flow.py

--- custom_components/qingping_cgs2/config_flow.py
+++ custom_components/qingping_cgs2/config_flow.py
@@ -33,14 +33,16 @@ class QingpingConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class QingpingOptionsFlowHandler(config_entries.OptionsFlow):
     """Меню настроек после установки."""
     def __init__(self, config_entry):
-        self.config_entry = config_entry
+        # В новых версиях HA config_entry является зарезервированным системным словом.
+        # Поэтому мы сохраняем его под именем _config_entry, чтобы избежать конфликта.
+        self._config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
-        # Берем текущее значение или ставим False, если его нет
-        use_history = self.config_entry.options.get(CONF_USE_HISTORY, False)
+        # Берем текущее значение
+        use_history = self._config_entry.options.get(CONF_USE_HISTORY, False)
 
         return self.async_show_form(
             step_id="init",
