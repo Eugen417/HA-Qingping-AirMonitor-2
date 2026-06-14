da17d283778e5e4eb3df10f2368fe2dda8e9412c
Eugen417 <eugen417@gmail.com>
2026-06-14 19:14:51 +0300

    Update __init__.py

--- custom_components/qingping_cgs2/__init__.py
+++ custom_components/qingping_cgs2/__init__.py
@@ -2,11 +2,16 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
-PLATFORMS = ["sensor", "binary_sensor"]
+PLATFORMS = ["sensor", "binary_sensor", "number", "select", "switch", "time", "button"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Запуск интеграции."""
     hass.data.setdefault(DOMAIN, {})
+    
+    hass.data[DOMAIN][entry.entry_id] = {
+        "coordinator": type("DummyCoordinator", (), {"data": {}})(),
+    }
+    
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
